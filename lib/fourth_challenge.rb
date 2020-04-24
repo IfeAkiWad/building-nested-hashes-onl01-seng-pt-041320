195eb559c83210e78abb0c2ae041be9907e2defd
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-24 06:27:35 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,19 +8,19 @@ def fourth_challenge
         {name: "Benvolio", age: "17", attutude: "worried"}
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        
+
       end
         #your hashes here!
       ]
    }
 }
 
-  
+
 
 end
